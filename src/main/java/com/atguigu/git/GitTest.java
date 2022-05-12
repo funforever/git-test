76bc536b18942913857d7fbe7968c532d452f666
jojo <1837625895@qq.com>
2022-05-12 21:18:22 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author shaojinwei
+ * @create 2022-05-12 21:13
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
