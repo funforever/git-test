eb29d9b80dcb4707f3467f21be8d79dcee98190e
jojo <1837625895@qq.com>
2022-05-12 21:21:37 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -2,10 +2,10 @@ package com.atguigu.git;
 
 /**
  * @author shaojinwei
- * @create 2022-05-12 21:13
  */
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
